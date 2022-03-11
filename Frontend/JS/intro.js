5b2e58b5038be56c07757d458037b695daef6b23
Ramón Alejandro Gutiérrez Calleja <alexgtz.up@gmail.com>
2022-03-10 19:04:43 -0600

    add: Práctica CSS

---
+++ Frontend/JS/intro.js
@@ -0,0 +1,14 @@
+console.log('\n*************** Variables ***************');
+var numero1;
+numero1  = 4;
+var numero2;
+numero2 = 2;
+console.log('Número 1: ' + numero1 + ' Número 2: ' + numero2 + ' = ' + (numero1+numero2));
+
+console.log('\n*************** Cadenas ***************');
+var frase1;
+frase1 = 'Holi comillas simples';
+var frase2;
+frase2 = "Holi comillas dobles";
+
+console.log(frase1);
