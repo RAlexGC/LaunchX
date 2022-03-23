9c5a282da683af636f48e10b65caa8563e6b5483
Alejandro Gutiérrez <alexgtz.up@gmail.com>
2022-03-22 22:18:26 -0600

    vue-tailwind

---
+++ Frontend/Vue_Vuex/bakery/src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="text-red-600 font-bold flex flex-col items-center">
+    <img alt="Vue logo" src="../assets/logo.png" class="w-20 h-20">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
