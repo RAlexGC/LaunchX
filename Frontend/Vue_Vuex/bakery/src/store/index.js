7fd107aae57a422fa8e2633e8093d8fc479cf4d7
Alejandro Gutiérrez <alexgtz.up@gmail.com>
2022-03-23 22:43:10 -0600

    Update práctica Vue-Vuex

--- Frontend/Vue_Vuex/bakery/src/store/index.js
+++ Frontend/Vue_Vuex/bakery/src/store/index.js
@@ -1,14 +1,20 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
 
 export default createStore({
   state: {
+    sabor1: " Hola ",
+    sabor2: " Holi ",
   },
-  getters: {
-  },
+  getters: {},
   mutations: {
+    updateSabor1(state, sabor1) {
+      state.sabor1 = sabor1;
+    },
+
+    updateSabor2(state, sabor2) {
+      state.sabor2 = sabor2;
+    },
   },
-  actions: {
-  },
-  modules: {
-  }
-})
+  actions: {},
+  modules: {},
+});
--- Frontend/Vue_Vuex/bakery/src/views/HomeView.vue
+++ Frontend/Vue_Vuex/bakery/src/views/HomeView.vue
@@ -1,17 +1,29 @@
 <template>
-  <div class="">
+  <div class="w-full h-screen">
     <Menu></Menu>
+    <Toppings></Toppings>
+    <Order></Order>
+    <TopOrder></TopOrder>
+    <Info></Info>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Menu from '@/components/Menu.vue'
+import Menu from "@/components/Menu.vue";
+import Info from "@/components/Info.vue";
+import Toppings from "@/components/Toppings.vue";
+import Order from "@/components/Order.vue";
+import TopOrder from "@/components/TopOrder.vue";
 
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   components: {
-    Menu
-  }
-}
+    Menu,
+    Info,
+    Toppings,
+    Order,
+    TopOrder,
+  },
+};
 </script>
